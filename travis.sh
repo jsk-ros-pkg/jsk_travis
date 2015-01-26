07926672ff64e99ee89645be2337df394552f594
Ryohei Ueda <garaemon@gmail.com>
2015-01-27 04:41:06 +0900

    Add $BUILD_PKGS to specify package to build

--- travis.sh
+++ travis.sh
@@ -64,11 +64,11 @@ source /opt/ros/$ROS_DISTRO/setup.bash # re-source setup.bash for setting enviro
 
 ### script: # All commands must exit with code 0 on success. Anything else is considered failure.
 # for catkin
-if [ "$BUILDER" == catkin ]; then catkin build -i -v --no-status --make-args $ROS_PARALLEL_JOBS            ; fi
-if [ "$BUILDER" == catkin ]; then catkin run_tests  ; fi
+if [ "$BUILDER" == catkin ]; then catkin build -i -v --no-status $BUILD_PKGS --make-args $ROS_PARALLEL_JOBS            ; fi
+if [ "$BUILDER" == catkin ]; then catkin run_tests $BUILD_PKGS ; fi
 if [ "$BUILDER" == catkin ]; then find build -name LastTest.log -exec echo "==== {} ====" \; -exec cat {} \;  ; fi
 if [ "$BUILDER" == catkin ]; then catkin clean -a                        ; fi
 if [ "$BUILDER" == catkin ]; then catkin config --install                ; fi
-if [ "$BUILDER" == catkin ]; then catkin build -i -v --no-status --make-args $ROS_PARALLEL_JOBS            ; fi
+if [ "$BUILDER" == catkin ]; then catkin build -i -v --no-status $BUILD_PKGS --make-args $ROS_PARALLEL_JOBS            ; fi
 if [ "$BUILDER" == catkin ]; then source install/setup.bash              ; fi
 if [ "$BUILDER" == catkin ]; then export EXIT_STATUS=0; for pkg in $TARGET_PKG; do [ "`find install/share/$pkg -iname '*.test'`" == "" ] && echo "[$pkg] No tests ware found!!!"  || find install/share/$pkg -iname "*.test" -print0 | xargs -0 -n1 rostest || export EXIT_STATUS=$?; done; [ $EXIT_STATUS == 0 ] ; fi
