598e934d40ac536f6eed16c4e5a76c1533a3705d
Kei Okada <kei.okada@gmail.com>
2015-04-02 16:29:26 -0700

    [.travis] FIX use latest travis which disable hrpsys doc generation

--- travis.sh
+++ travis.sh
@@ -96,7 +96,7 @@ if [ "$USE_DEB" == false ]; then $ROSWS set $REPOSITORY_NAME http://github.com/$
 ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
 if [ "$USE_DEB" == source -a -e $REPOSITORY_NAME/setup_upstream.sh ]; then $ROSWS init .; $REPOSITORY_NAME/setup_upstream.sh -w ~/ros/ws_$REPOSITORY_NAME ; $ROSWS update; fi
 # disable hrpsys/doc generation
-find  -ipath "*/hrpsys-base-source/CMakeLists.txt" -exec sed -i s'@if(ENABLE_DOXYGEN)@if(0)@' {} \;
+find . -ipath "*/hrpsys/CMakeLists.txt" -exec sed -i s'@if(ENABLE_DOXYGEN)@if(0)@' {} \;
 
 cd ../
 # Install dependencies for source repos
