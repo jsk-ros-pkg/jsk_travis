e092d24cf1758c6c62c04d8a7fb11ab521c76f13
Kei Okada <kei.okada@gmail.com>
2015-04-23 20:06:11 +0900

    [upload-docs.sh] fix :tell them who am i, push data

--- upload-docs.sh
+++ upload-docs.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-git clone http://github.com/jsk-ros-pkg/euslisp-docs ~/euslisp-docs
+set -x
+export GIT_COMMITTER_NAME=$GIT_NAME
+export GIT_COMMITTER_EMAIL=$GIT_EMAIL
+export GIT_AUTHOR_NAME=$GIT_NAME
+export GIT_AUTHOR_EMAIL=$GIT_EMAIL
+export REPOSITORY_NAME=${PWD##*/}
+git clone https://github.com/jsk-ros-pkg/euslisp-docs.git ~/euslisp-docs
+cd ~/euslisp-docs/docs
 for pkg in ~/ros/ws_$REPOSITORY_NAME/build/*; do
-    cd $pkg;
-    if ls *.md > /dev/null 2>&1; then
+    if ls $pkg/*.md > /dev/null 2>&1; then
         name=`basename $pkg`;
         echo $name;
-        ls -al *.md
-        mkdir -p ~/euslisp-docs/docs/$name/
-        cp *.md ~/euslisp-docs/docs/$name/
-        git add ~/euslisp-docs/docs/$name/*.md
+        ls -al $pkg/*.md
+        mkdir -p $name
+        cp $pkg/*.md $name/
+        git add $name/*.md
         git commit -m "update documentation for $name" -a
     fi
 done
+git push --quiet https://$GH_TOKEN@github.com/jsk-ros-pkg/euslisp-docs.git master
