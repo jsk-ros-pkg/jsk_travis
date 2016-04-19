623949d0b8452402041b149fb3b9488f21cf632c
Kei Okada <kei.okada@gmail.com>
2016-04-19 16:01:13 +0900

    travis.sh : could not install catkin-tools from apt, use pip instaed

--- travis.sh
+++ travis.sh
@@ -70,7 +70,12 @@ sudo -E sh -c 'echo "deb $ROS_REPOSITORY_PATH `lsb_release -cs` main" > /etc/apt
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 lsb_release -a
 sudo apt-get update -q || echo Ignore error of apt-get update
-sudo apt-get install -y --force-yes -q -qq python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-rosbash ros-$ROS_DISTRO-rospack ccache
+if [ "$ROS_DISTRO" == "hydro" ]; then # https://github.com/catkin/catkin_tools/issues/336
+    sudo pip install catkin-tools
+else
+    sudo apt-get install -y python-catkin-tools
+fi
+sudo apt-get install -y --force-yes -q -qq python-rosdep python-wstool ros-$ROS_DISTRO-rosbash ros-$ROS_DISTRO-rospack ccache
 # setup ccache
 sudo ln -s /usr/bin/ccache /usr/local/bin/gcc
 sudo ln -s /usr/bin/ccache /usr/local/bin/g++
