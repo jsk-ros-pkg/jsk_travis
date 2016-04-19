9bae7dc6fecb0efa08bf6b694daeafd291cb4ccb
Kei Okada <kei.okada@gmail.com>
2016-04-19 13:00:46 +0900

    order of --from-paths was not correct

--- rosdep-install.sh
+++ rosdep-install.sh
@@ -3,6 +3,8 @@
 trap 'find -L . -name manifest.xml.deprecated | xargs -n 1 -i dirname {} | xargs -n 1 -i mv `pwd`/{}/manifest.xml.deprecated `pwd`/{}/manifest.xml' 1 2 3 15
 
 find -L . -name package.xml -exec dirname {} \; | xargs -n 1 -i find {} -name manifest.xml | xargs -n 1 -i mv {} {}.deprecated # rename manifest.xml for rosdep install
-rosdep install -y --rosdistro $ROS_DISTRO $ROSDEP_ADDITIONAL_OPTIONS --from-paths . `for i in ${ROS_PACKAGE_PATH//:/\ /}; do if [ -e $i ]; then echo -n $i' '; fi; done`
+PACKAGE_PATH_LIST=(${ROS_PACKAGE_PATH//:/\ /})
+ROS_PACKAGE_PATH_REVERSED=`for ((i=${#PACKAGE_PATH_LIST[@]}-1; i>=0; i--)); do if [ -e ${PACKAGE_PATH_LIST[$i]} ]; then echo -n ${PACKAGE_PATH_LIST[$i]}' '; fi; done`
+rosdep install -y --rosdistro $ROS_DISTRO $ROSDEP_ADDITIONAL_OPTIONS --from-paths ${ROS_PACKAGE_PATH_REVERSED} .
 find -L . -name manifest.xml.deprecated | xargs -n 1 -i dirname {} | xargs -n 1 -i mv `pwd`/{}/manifest.xml.deprecated `pwd`/{}/manifest.xml
 
