f1be2861281fbe24675bf0ea6b715765381ba7d7
Kei Okada <kei.okada@gmail.com>
2016-04-19 14:01:31 +0900

    rosdep-install.sh: try 3 times

--- rosdep-install.sh
+++ rosdep-install.sh
@@ -5,6 +5,17 @@ trap 'find -L . -name manifest.xml.deprecated | xargs -n 1 -i dirname {} | xargs
 find -L . -name package.xml -exec dirname {} \; | xargs -n 1 -i find {} -name manifest.xml | xargs -n 1 -i mv {} {}.deprecated # rename manifest.xml for rosdep install
 PACKAGE_PATH_LIST=(${ROS_PACKAGE_PATH//:/\ /})
 ROS_PACKAGE_PATH_REVERSED=`for ((i=${#PACKAGE_PATH_LIST[@]}-1; i>=0; i--)); do if [ -e ${PACKAGE_PATH_LIST[$i]} ]; then echo -n ${PACKAGE_PATH_LIST[$i]}' '; fi; done`
-rosdep install -y --rosdistro $ROS_DISTRO $ROSDEP_ADDITIONAL_OPTIONS --from-paths ${ROS_PACKAGE_PATH_REVERSED} .
+EXIT_STATUS=1
+COUNT=0
+while [ $EXIT_STATUS == 1 -a $COUNT -lt 3 ] ; do  # try 3 times with "Continue installing despite errors." option
+    COUNT=$((COUNT + 1))
+    pwd
+    rosdep install -y --rosdistro $ROS_DISTRO --from-paths ${ROS_PACKAGE_PATH_REVERSED} .
+    EXIT_STATUS=$?
+    echo "adfafda"
+    [ $EXIT_STATUS == 0 ] || sleep 30
+done
+[ $EXIT_STATUS == 0 ] || rosdep install -y --rosdistro $ROS_DISTRO $ROSDEP_ADDITIONAL_OPTIONS --from-paths ${ROS_PACKAGE_PATH_REVERSED} .
+
 find -L . -name manifest.xml.deprecated | xargs -n 1 -i dirname {} | xargs -n 1 -i mv `pwd`/{}/manifest.xml.deprecated `pwd`/{}/manifest.xml
 
