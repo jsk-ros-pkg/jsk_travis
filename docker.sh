3d40016af174b5c510cd451b6d7bf05bf470a2f7
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-08-31 00:53:47 +0900

    support docker on travis

---
+++ docker.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# travis related functions (copied from https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/templates/header.sh)
+export ANSI_RED="\033[31;1m"
+export ANSI_GREEN="\033[32;1m"
+export ANSI_BLUE="\033[34;1m"
+export ANSI_RESET="\033[0m"
+export ANSI_CLEAR="\033[0K"
+
+function travis_time_start {
+    set +x
+    TRAVIS_START_TIME=$(date +%s%N)
+    TRAVIS_TIME_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+    TRAVIS_FOLD_NAME=$1
+    echo -e "${ANSI_CLEAR}traivs_fold:start:$TRAVIS_FOLD_NAME"
+    echo -e "${ANSI_CLEAR}traivs_time:start:$TRAVIS_TIME_ID${ANSI_BLUE}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${ANSI_RESET}"
+    set -x
+}
+export -f travis_time_start
+
+function travis_time_end {
+    set +x
+    _COLOR=${1:-32}
+    TRAVIS_END_TIME=$(date +%s%N)
+    TIME_ELAPSED_SECONDS=$(( ($TRAVIS_END_TIME - $TRAVIS_START_TIME)/1000000000 ))
+    echo -e "traivs_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n${ANSI_CLEAR}"
+    echo -e "traivs_fold:end:$TRAVIS_FOLD_NAME\e[${_COLOR}m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<${ANSI_RESET}"
+    echo -e "${ANSI_CLEAR}\e[${_COLOR}mFunction $TRAVIS_FOLD_NAME takes $(( $TIME_ELAPSED_SECONDS / 60 )) min $(( $TIME_ELAPSED_SECONDS % 60 )) sec${ANSI_RESET}"
+    set -x
+}
+export -f travis_time_end
+
+function travis_wait {
+  local timeout=$1
+
+  if [[ $timeout =~ ^[0-9]+$ ]]; then
+    # looks like an integer, so we assume it's a timeout
+    shift
+  else
+    # default value
+    timeout=20
+  fi
+
+  local cmd="$@"
+  local log_file=travis_wait_$$.log
+
+  $cmd &>$log_file &
+  local cmd_pid=$!
+
+  travis_jigger $! $timeout $cmd &
+  local jigger_pid=$!
+  local result
+
+  {
+    wait $cmd_pid 2>/dev/null
+    result=$?
+    ps -p$jigger_pid &>/dev/null && kill $jigger_pid
+  }
+
+  if [ $result -eq 0 ]; then
+    echo -e "\n${ANSI_GREEN}The command $cmd exited with $result.${ANSI_RESET}"
+  else
+    echo -e "\n${ANSI_RED}The command $cmd exited with $result.${ANSI_RESET}"
+  fi
+
+  echo -e "\n${ANSI_GREEN}Log:${ANSI_RESET}\n"
+  cat $log_file
+
+  return $result
+}
+export -f travis_wait
+
+function travis_jigger {
+  # helper method for travis_wait()
+  local cmd_pid=$1
+  shift
+  local timeout=$1 # in minutes
+  shift
+  local count=0
+
+  # clear the line
+  echo -e "\n"
+
+  while [ $count -lt $timeout ]; do
+    count=$(($count + 1))
+    echo -ne "Still running ($count of $timeout): $@\r"
+    sleep 60
+  done
+
+  echo -e "\n${ANSI_RED}Timeout (${timeout} minutes) reached. Terminating \"$@\"${ANSI_RESET}\n"
+  kill -9 $cmd_pid
+}
+export -f travis_jigger
+
+# end of travis related functions
+
+travis_time_start setup_docker
+
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$(which sudo)" = "" ]; then
+  apt-get -y -qq update
+  apt-get -y -qq install sudo
+fi
+# install fundamental packages
+sudo -E apt-get -y -qq update
+sudo -E apt-get -y -qq install apt-utils build-essential git lsb-release python-pip python-setuptools wget
+
+# add user for testing
+adduser --disabled-password --gecos "" travis
+adduser travis sudo
+chown -R travis:travis $HOME
+echo "travis ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+
+# setup virtual display
+sudo -E apt-get -y -qq install mesa-utils xserver-xorg-video-dummy xvfb
+export DISPLAY=:0
+sudo -E Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config dummy.xorg.conf $DISPLAY &
+sleep 3
+glxinfo | grep GLX
+
+# ensure setting testing environment same as travis
+export USE_JENKINS=false
+export USE_TRAVIS=true
+
+travis_time_end
+
+# run tests
+su travis -c 'cd $CI_SOURCE_PATH; source .travis/travis.sh'
--- travis.sh
+++ travis.sh
@@ -2,13 +2,22 @@
 
 set -x
 
+export CI_SOURCE_PATH=$(pwd)
+export REPOSITORY_NAME=${PWD##*/}
+
+ANSI_RED="\033[31;1m"
+ANSI_GREEN="\033[32;1m"
+ANSI_BLUE="\033[34;1m"
+ANSI_RESET="\033[0m"
+ANSI_CLEAR="\033[0K"
+
 function travis_time_start {
     set +x
     TRAVIS_START_TIME=$(date +%s%N)
     TRAVIS_TIME_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
     TRAVIS_FOLD_NAME=$1
-    echo -e "\e[0Ktraivs_fold:start:$TRAVIS_FOLD_NAME"
-    echo -e "\e[0Ktraivs_time:start:$TRAVIS_TIME_ID\e[34m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+    echo -e "${ANSI_CLEAR}traivs_fold:start:$TRAVIS_FOLD_NAME"
+    echo -e "${ANSI_CLEAR}traivs_time:start:$TRAVIS_TIME_ID${ANSI_BLUE}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${ANSI_RESET}"
     set -x
 }
 function travis_time_end {
@@ -16,9 +25,9 @@ function travis_time_end {
     _COLOR=${1:-32}
     TRAVIS_END_TIME=$(date +%s%N)
     TIME_ELAPSED_SECONDS=$(( ($TRAVIS_END_TIME - $TRAVIS_START_TIME)/1000000000 ))
-    echo -e "traivs_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n\e[0K"
-    echo -e "traivs_fold:end:$TRAVIS_FOLD_NAME\e[${_COLOR}m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-    echo -e "\e[0K\e[${_COLOR}mFunction $TRAVIS_FOLD_NAME takes $(( $TIME_ELAPSED_SECONDS / 60 )) min $(( $TIME_ELAPSED_SECONDS % 60 )) sec\e[0m"
+    echo -e "traivs_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n${ANSI_CLEAR}"
+    echo -e "traivs_fold:end:$TRAVIS_FOLD_NAME\e[${_COLOR}m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<${ANSI_RESET}"
+    echo -e "${ANSI_CLEAR}\e[${_COLOR}mFunction $TRAVIS_FOLD_NAME takes $(( $TIME_ELAPSED_SECONDS / 60 )) min $(( $TIME_ELAPSED_SECONDS % 60 )) sec${ANSI_RESET}"
     set -x
 }
 
@@ -28,6 +37,34 @@ function travis_time_end {
 # Deprecated environmental variables
 [ ! -z $BUILDER -a "$BUILDER" != catkin ] && ( echo "ERROR: $BUILDER is not supported. BUILDER env is deprecated and only 'catkin' is supported for the build."; exit 1; )
 [ ! -z $ROSWS -a "$ROSWS" != wstool ] && ( echo "ERROR: $ROSWS is not supported. ROSWS env is deprecated and only 'wstool' is supported for workspace management."; exit 1; )
+
+# docker on travis
+if [ "$USE_DOCKER" = true ]; then
+  if [ "$DOCKER_IMAGE" = "" ]; then
+    case $ROS_DISTRO in
+      hydro) DISTRO=precise;;
+      indigo|jade) DISTRO=trusty;;
+      kinetic) DISTRO=xenial;;
+      *) DISTRO=trusty;;
+    esac
+    export DOCKER_IMAGE=ubuntu:$DISTRO
+  fi
+  docker run -it -v $HOME:$HOME \
+    -e TRAVIS_BRANCH -e TRAVIS_COMMIT -e TRAVIS_JOB_ID -e TRAVIS_OS_NAME -e TRAVIS_PULL_REQUEST -e TRAVIS_REPO_SLUG \
+    -e CI_SOURCE_PATH -e HOME -e REPOSITORY_NAME \
+    -e BUILD_PKGS -e TARGET_PKGS -e TEST_PKGS \
+    -e BEFORE_SCRIPT -e BUILDER -e EXTRA_DEBS -e USE_DEB \
+    -e ROS_DISTRO -e ROS_LOG_DIR -e ROS_REPOSITORY_PATH -e ROSWS \
+    -e CATKIN_TOOLS_BUILD_OPTIONS -e CATKIN_TOOLS_CONFIG_OPTIONS \
+    -e CATKIN_PARALLEL_JOBS -e CATKIN_PARALLEL_TEST_JOBS \
+    -e ROS_PARALLEL_JOBS -e ROS_PARALLEL_TEST_JOBS \
+    -e ROSDEP_ADDITIONAL_OPTIONS -e ROSDEP_UPDATE_QUIET \
+    -e SUDO_PIP -e USE_PYTHON_VIRTUALENV \
+    -e NOT_TEST_INSTALL \
+    -t $DOCKER_IMAGE bash -c 'cd $CI_SOURCE_PATH; .travis/docker.sh'
+  DOCKER_EXIT_CODE=$?
+  exit $DOCKER_EXIT_CODE
+fi
 
 if [ "$USE_TRAVIS" != "true" ] && [ "$ROS_DISTRO" == "indigo" -o "$ROS_DISTRO" == "jade" -o "$ROS_DISTRO" == "kinetic" -o "${USE_JENKINS}" == "true" ] && [ "$TRAVIS_JOB_ID" ]; then
     pip install --user python-jenkins -q
@@ -67,7 +104,6 @@ if [ "$(git diff origin/master HEAD .travis)" != "" ] ; then
 fi
 
 travis_time_end is_jsk_travis_upgraded
-
 
 travis_time_start setup_ros
 
