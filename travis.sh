6ced86a490f14f6a47a35d02763fcc96104e4191
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-08-02 20:49:13 +0900

    on hydro, use 0.11.5

--- travis.sh
+++ travis.sh
@@ -143,6 +143,9 @@ wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 lsb_release -a
 sudo apt-get update -q || echo Ignore error of apt-get update
 sudo apt-get install -y --force-yes -q -qq python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-rosbash ros-$ROS_DISTRO-rospack ccache
+if [ "$ROS_DISTRO" == "hydro" ]; then
+    sudo pip install rosdep==0.11.5
+fi
 # setup catkin-tools option
 if [ ! "$CATKIN_TOOLS_BUILD_OPTIONS" ]; then
   if [[ "$(pip show catkin-tools | grep '^Version:' | awk '{print $2}')" =~ 0.3.[0-9]+ ]]; then
