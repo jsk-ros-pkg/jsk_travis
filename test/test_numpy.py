bad8c1e826b8656cc3f2c058d1df74b1f15bffd9
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-05-27 10:32:22 +0900

    show debug message to stderr

--- test/test_numpy.py
+++ test/test_numpy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import sys
 import unittest
 import numpy
@@ -9,8 +11,8 @@ class TestNumpy(unittest.TestCase):
 
     def test_numpy(self):
 
-        print("NumPy : ", numpy.version.full_version)
-        print("Python: ", sys.version)
+        print("NumPy : " + numpy.version.full_version, file=sys.stderr)
+        print("Python: " + sys.version, file=sys.stderr)
         self.assertTrue('true')
 
 if __name__ == '__main__':
