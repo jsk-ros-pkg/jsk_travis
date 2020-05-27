b14d17cbc7ede483515f07f4a27b5de9c6a5dcb4
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-05-27 13:41:02 +0900

    use scipy instead of numpy, because numpy is already installed

--- test/test_scipy.py
+++ test/test_scipy.py
@@ -5,13 +5,13 @@ from __future__ import print_function
 
 import sys
 import unittest
-import numpy
+import scipy
 
 class TestNumpy(unittest.TestCase):
 
     def test_numpy(self):
 
-        print("NumPy : " + numpy.version.full_version, file=sys.stderr)
+        print("NumPy : " + scipy.version.full_version, file=sys.stderr)
         print("Python: " + sys.version, file=sys.stderr)
         self.assertTrue('true')
 
