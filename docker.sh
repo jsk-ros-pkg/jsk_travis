9fc19ebe7c66da58de353eb802db4a8b0547f61c
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-06-27 16:16:31 +0900

    install python3-pip for 20.04

--- docker.sh
+++ docker.sh
@@ -108,7 +108,7 @@ fi
 sudo -E apt-get -y -qq update
 sudo -E apt-get -y -qq install apt-utils build-essential curl git lsb-release wget
 # 20.04 does not have pip, so install get-pip.py
-sudo -E apt-get -y -qq install python-pip python-setuptools || (sudo -E apt-get -y -qq install python; curl https://bootstrap.pypa.io/get-pip.py | sudo -E python)
+sudo -E apt-get -y -qq install python-pip python-setuptools || (sudo -E apt-get -y -qq install python; curl https://bootstrap.pypa.io/get-pip.py | sudo -E python; sudo -E apt-get -y -qq install python3-pip)
 
 # add user for testing
 adduser --disabled-password --gecos "" travis
